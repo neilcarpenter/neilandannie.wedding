81e6db92d3843f3de5a5ac5ee431007d56383d8d
Neil Carpenter <neilanthonycarpenter@gmail.com>
2016-09-08 00:55:42 +0100

    Add html minification

--- script/generateStaticMarkup.js
+++ script/generateStaticMarkup.js
@@ -1,11 +1,21 @@
 import fs from 'fs'
+import minifier from 'html-minifier'
 
 const sourceDir = './app/pages'
 const targetDir = './app/build'
 
+const minifyOpts = {
+  collapseWhitespace: true,
+  minifyCSS: true,
+  minifyJS: true,
+  removeAttributeQuotes: true,
+  removeRedundantAttributes: true
+}
+
 function compileAndSavePage(name) {
   const pageModule  = require('../app/pages/' + name)
   const pageContent = pageModule.getPageHtml()
+  const pageContentMin = minifier.minify(pageContent, minifyOpts)
   const pagePath = name === 'home' ? '' : `${name.replace(/_/g, '/')}`
   const target = `${targetDir}/${pagePath}`
 
@@ -14,7 +24,7 @@ function compileAndSavePage(name) {
   }
 
   console.log("Writing static HTML for page %s to %s", name, target)
-  fs.writeFileSync(`${target}/index.html`, pageContent)
+  fs.writeFileSync(`${target}/index.html`, pageContentMin)
 }
 
 fs.readdir(sourceDir, (err, names) => {
